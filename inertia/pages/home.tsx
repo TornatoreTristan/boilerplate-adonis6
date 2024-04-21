baa2a68ef99de1508086dfb39c548740dcaa27d2
AgenceRevontuli <tristan@revontuli.fr>
2024-04-21 16:07:13 +0200

    Page: Add UI Elements

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -5,8 +5,10 @@ export default function Home(props: { version: number; versionBoilerplate: strin
     <>
       <Head title="Homepage" />
 
-      <div className="container">
-        <div className="title">AdonisJS {props.version} x Inertia x React</div>
+      <div className="flex flex-col gap-8 h-screen justify-center items-center">
+        <div className="text-xl">
+          <h1>AdonisJS {props.version} x Inertia x React</h1>
+        </div>
 
         <span>BoilerPlate {props.versionBoilerplate}</span>
       </div>
